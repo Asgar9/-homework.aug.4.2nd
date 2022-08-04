8ca068aa18b73a1bdb4f06c2768b987dca0a0747
Asgar9 <asgaryas@code.edu.az>
2022-08-04 11:13:01 +0400

    area mothod

---
+++ area/area/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OVERLOAD-1");
+            Console.WriteLine(Area(int.Parse(Console.ReadLine())));
+            Console.WriteLine("OVERLAOD-2");
+            Console.WriteLine(Area(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+            Console.WriteLine("OVERLAOD-3");
+            Console.WriteLine(Area(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+            Console.WriteLine("OVERLAOD-4");
+            Console.WriteLine(Area(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+        }
+        static int Area(int r)
+        {
+           int   s = 3 * r * r;
+            return s;
+            
+        }
+        static int Area(int a, int b)
+        {
+            
+           int s = a * b;
+            return s;
+        }
+        static int Area(int a,int b,int c)
+        {
+            int s = 2 * (a * b + a * c + b * c);
+            return s;
+        }
+     
+        static int Area(int a, int c, int b,int r)
+        {
+            int p = (a + b + c) / 2;
+            int s = p * r;
+            return s;
+        }
+    }
+}
